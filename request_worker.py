25b796f8a262735bf0df77946c08b8594e9b529e
tihontormoz <132567818+tihontormoz@users.noreply.github.com>
2023-05-04 14:07:04 +0300

    Update request_worker.py

--- request_worker.py
+++ request_worker.py
@@ -1,6 +1,9 @@
 import requests
 from secret import zabbix_token
+from config import need_db, zabbix_server_api
 import json
+if need_db == True:
+    import db_worker
 
 class RequestWorker:
     token = zabbix_token
@@ -16,19 +19,19 @@ class RequestWorker:
     def get_problems(self, problems_params):
         request_params = self.basic_params.copy()
         request_params['params'] = problems_params
-        response = requests.post(url='http://docs.m-g-s.ru:21088/api_jsonrpc.php', data=json.dumps(request_params), headers=self.headers)
+        response = requests.post(url=zabbix_server_api, data=json.dumps(request_params), headers=self.headers)
         print(response.json())
         request_params['method'] = "item.get"
         request_params['params'] = {'itemids': [problem['objectid'] if problem['object'] == '4' else 0 for problem in response.json()['result']]}
-        response_host = requests.post(url='http://docs.m-g-s.ru:21088/api_jsonrpc.php', data=json.dumps(request_params), headers=self.headers)
+        response_host = requests.post(url=zabbix_server_api, data=json.dumps(request_params), headers=self.headers)
         res = response_host.json()['result']
         request_params['method'] = "trigger.get"
         request_params['params'] = {'triggerids': [problem['objectid'] if problem['object'] == '0' else 0 for problem in response.json()['result']]}
-        response_host = requests.post(url='http://docs.m-g-s.ru:21088/api_jsonrpc.php', data=json.dumps(request_params), headers=self.headers)
+        response_host = requests.post(url=zabbix_server_api, data=json.dumps(request_params), headers=self.headers)
         res += response_host.json()['result']
         request_params['method'] = "discoveryrule.get"
         request_params['params'] = {'itemids': [problem['objectid'] if problem['object'] == '5' else 0 for problem in response.json()['result'] ]}
-        response_host = requests.post(url='http://docs.m-g-s.ru:21088/api_jsonrpc.php', data=json.dumps(request_params), headers=self.headers)
+        response_host = requests.post(url=zabbix_server_api, data=json.dumps(request_params), headers=self.headers)
         res += response_host.json()['result']
         print(len(res))
         print(len(response.json()['result']))
